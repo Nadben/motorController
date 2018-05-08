c9133184757a84f09036f83f511e129c45d18277
Nadben <33803808+Nadben@users.noreply.github.com>
2018-05-08 19:36:35 -0400

    Add files via upload
    Manette qui transmet les donnees

---
+++ Nano_transmit.ino
@@ -0,0 +1,16 @@
+int potentiometer=A0;
+
+void setup() {
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  int potval = analogRead(potentiometer);
+  //Serial.println(potval);
+  byte potval_1 = map(potval,330,645,0,255);
+  //Serial.print(potval_1);
+  Serial.write(potval_1);
+  delay(20);
+
+}
