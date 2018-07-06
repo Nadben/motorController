7999f73f05fbb83d921a1fc04df3cd85acef7454
Nadben <33803808+Nadben@users.noreply.github.com>
2018-07-06 16:16:21 -0400

    v2 pour utilisation avec Wii nunchuck
    --Version non testé

---
+++ Nano_transmit_V2.ino
@@ -0,0 +1,54 @@
+#include <Wire.h>
+
+uint8_t dataStream[6]; //array de 6 bytes
+int counter = 0 ;
+
+//Dans le setup je dois lire la caibration actuel
+//du joystick Y puis de sortir les valeurs suivantes
+uint8_t joysticky_max = 0;//rentrer les bonnes valeurs
+uint8_t joysticky_min = 0;//rentrer les bonnes valeurs
+uint8_t joysticky_cent = 0;//rentrer les bonnes valeurs
+
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin(); 
+  //slave address or 0x54
+  //initialize the nunchuk without encryption
+  Wire.beginTransmission(0x52);
+  Wire.write(0xF0);
+  Wire.write(0x55);
+  Wire.endTransmission();
+
+  Wire.beginTransmission(0x52);
+  Wire.write(0xFB);
+  Wire.write(0x00);
+  Wire.endTransmission();
+
+  
+
+}
+
+void loop() {
+  //boucle while avec counter qui enregistre les valeurs
+  //lues et qui renvoient l'array
+  Wire.requestFrom(0x52,6);
+  counter = 0;
+  while(Wire.available()){
+    dataStream[counter++] = Wire.read();
+   }
+
+  if(counter >= 5){
+    //lit la valeur potvaly
+    uint8_t potvaly = dataStream[1];
+    Serial.println(potvaly);
+    
+  
+    //remplacer les valeurs 330 et 645 par les 
+    //valeurs min et max de la calibration
+    byte potval_1 = map(potvaly,330,645,0,255);
+    //Serial.print(potval_1);
+    Serial.write(potval_1);
+    delay(20);
+  }
+}
