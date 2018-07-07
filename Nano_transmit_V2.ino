4674d36a40bb85efb2ed9279bd91cc5126a2cdba
Nadben <33803808+Nadben@users.noreply.github.com>
2018-07-07 12:17:30 -0400

    V2 Nunchuk transmitter - tested version

--- Nano_transmit_V2.ino
+++ Nano_transmit_V2.ino
@@ -1,48 +1,66 @@
 #include <Wire.h>
 
-uint8_t dataStream[6]; //array de 6 bytes
+byte dataStream[6]; //array de 6 bytes
 int counter = 0 ;
 
-//Dans le setup je dois lire la caibration actuel
-//du joystick Y puis de sortir les valeurs suivantes
-uint8_t joysticky_max = 0;//rentrer les bonnes valeurs
-uint8_t joysticky_min = 0;//rentrer les bonnes valeurs
-uint8_t joysticky_cent = 0;//rentrer les bonnes valeurs
-
-
 void setup() {
-  Serial.begin(9600);
+
   Wire.begin(); 
-  //slave address or 0x54
+
   //initialize the nunchuk without encryption
   Wire.beginTransmission(0x52);
-  Wire.write(0xF0);
-  Wire.write(0x55);
+  Wire.write((uint8_t)0xF0);
+  Wire.write((uint8_t)0x55);
   Wire.endTransmission();
 
   Wire.beginTransmission(0x52);
-  Wire.write(0xFB);
-  Wire.write(0x00);
+  Wire.write((uint8_t)0xFB);
+  Wire.write((uint8_t)0x00);
   Wire.endTransmission();
+
+  Serial.begin(19200);
 
   
 
 }
 
+void send_request(){
+
+  Wire.beginTransmission(0x52);
+  Wire.write((uint8_t)0x00);
+  Wire.endTransmission();
+  
+}
+
 void loop() {
-  //boucle while avec counter qui enregistre les valeurs
-  //lues et qui renvoient l'array
   Wire.requestFrom(0x52,6);
   counter = 0;
   while(Wire.available()){
     dataStream[counter++] = Wire.read();
-   }
-
+  }
+  send_request(); //sans ca ca fuck up everything
+  
   if(counter >= 5){
     //lit la valeur potvaly
-    uint8_t potvaly = dataStream[1];
-    Serial.println(potvaly);
-    Serial.write(potvaly);
-    delay(20);
+    int potvalx = dataStream[0];
+    int potvaly = dataStream[1];
+    bool z_button = (dataStream[5] >> 0) & 1; 
+    bool c_button = (dataStream[5] >> 1) & 1;
+   
+
+    Serial.print("Joy y:");
+    Serial.print(potvaly, DEC);
+    Serial.println();
+    Serial.println(c_button);
+    Serial.println(z_button);
+    Serial.println();
+
   }
+  delay(20);
 }
+
+
+
+
+
+
